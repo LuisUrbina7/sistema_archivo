9f0392184406b224c1b53a2ba6d9f850d19a85f9
LuisUrbina7 <luisaugusto.urbina7@gmail.com>
2022-11-23 22:56:49 -0600

    23/11 noche

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,14 +8,52 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Inicio') }}</title>
     <!-- Styles -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="{{asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/dashboard.css')}}">
 
 
 </head>
-<body>
-    <div id="app">
 
-        <main class="py-4">
+<body>
+   
+
+        <main class="py-4 form-signin text-center">
+           
+                <form method="POST" action="{{ route('login') }}">
+                @csrf
+                    <img class="mb-4" src="" alt="" width="72" height="57">
+                    <h1 class="h3 mb-3 fw-normal">Inicio</h1>
+    
+                    <div class="form-floating">
+                        <input id="username" name="username" class="form-control" id="floatingInput" placeholder="v00000000" value="{{ old('username') }}" autofocus>
+                        <label for="floatingInput">Usuario</label>
+                        @error('username')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="contraseña" name="password" value="{{ old('password') }}">
+                        <label for="floatingPassword" >Contraseña</label>
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+    
+                    <div class="checkbox mb-3">
+                        <label>
+                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}> Recuérdame
+                        </label>
+                    </div>
+                    <button class="w-100 btn btn-lg btn-primary" type="submit">Iniciar</button>
+                    <p class="mt-5 mb-3 text-muted">&copy; 2021–2022</p>
+                </form>
+            
+
+
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-md-8">
@@ -80,8 +118,7 @@
                 </div>
             </div>
         </main>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+        <script src="{{asset('bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 </body>
 
 </html>
